35af5a395094ba666bf2d650b79b68703f16e084
marufa <mmprogga@gmail.com>
2014-10-23 14:50:42 +0600

    Create add_new_properties.php

---
+++ add_new_properties.php
@@ -0,0 +1,14 @@
+
+<p id="demo"></p>
+
+<script>
+var person = {
+    firstname:"John",
+    lastname:"Doe",
+    age:50,
+    eyecolor:"blue"
+};
+person.nationality = "English";
+document.getElementById("demo").innerHTML =
+person.firstname + " is " + person.nationality + ".";
+</script>
